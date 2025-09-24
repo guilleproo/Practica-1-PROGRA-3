11bd5fd1b8a467d48808455fa13002f992d12a56
guilleproo <guilleacg21@gmial.com>
2025-09-23 21:30:18 -0600

    Seguimiento del codigo del profe

--- About.aspx.designer.vb
+++ About.aspx.designer.vb
@@ -12,4 +12,40 @@ Option Explicit On
 
 
 Partial Public Class About
+
+    '''<summary>
+    '''txt_nombre control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents txt_nombre As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''txt_apellido control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents txt_apellido As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''txt_edad control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents txt_edad As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''btn_guardar control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents btn_guardar As Global.System.Web.UI.WebControls.Button
 End Class
--- About.aspx.vb
+++ About.aspx.vb
@@ -1,7 +1,15 @@
-﻿Public Class About
+﻿Imports System.Web.ModelBinding
+
+Public Class About
     Inherits Page
+    Public persona As New Persona()
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btn_guardar_Click(sender As Object, e As EventArgs)
+        persona.Nombre = txt_nombre.Text
 
     End Sub
 End Class
